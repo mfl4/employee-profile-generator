c0eb1c81f66f6d9fdc45f70404da60c47af7e33f
mfl4 <91680736+mfl4@users.noreply.github.com>
2026-07-10 09:04:21 +0700

    feat: add string slicing examples using employee code

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ position = 'Data Analyst'
 salary = 75000
 employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {position} | Salary: ${salary}'
 print(employee_card)
+
+employee_code = 'DEV-2026-JD-001'
+department = employee_code[0:3]
+print(department)
+year_code = employee_code[4:8]
+print(year_code)
+initials = employee_code[9:11]
+print(initials)
+last_three = employee_code[-3:]
+print(last_three)
